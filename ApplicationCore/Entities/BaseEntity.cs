8dd13cb60918f97796aa03b70cde2d859ccb54a0
Edgias Mudzukwa <mudzukwae@outlook.com>
2020-09-16 20:11:46 +0200

    Add project files.

---
+++ ApplicationCore/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Edgias.Inventory.Management.ApplicationCore.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+
+        public DateTimeOffset CreatedDate { get; set; } = DateTimeOffset.Now;
+
+        public DateTimeOffset LastModifiedDate { get; set; } = DateTimeOffset.Now;
+    }
+}
---
+++ ApplicationCore/Entities/ProductCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace Edgias.Inventory.Management.ApplicationCore.Entities
+{
+    public class ProductCategory : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
