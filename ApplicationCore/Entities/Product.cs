2c364903afd9a2a8fffeb2fb0074fdd2843b2d76
Edgias Mudzukwa <mudzukwae@outlook.com>
2020-09-18 11:18:19 +0200

    Added product entity and db configurations for product and product category entities.

---
+++ ApplicationCore/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Edgias.Inventory.Management.ApplicationCore.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string ProductCode { get; set; }
+
+        public string Description { get; set; }
+
+        public Guid ProductCategoryId { get; set; }
+
+        public ProductCategory ProductCategory { get; set; }
+    }
+}
