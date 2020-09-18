b6d282328b5d038c1e7dc775d1fae25720b35742
Edgias Mudzukwa <mudzukwae@outlook.com>
2020-09-18 11:17:30 +0200

    Added location entity and db configuration.

---
+++ ApplicationCore/Entities/Address.cs
@@ -0,0 +1,29 @@
+﻿namespace Edgias.Inventory.Management.ApplicationCore.Entities
+{
+    public class Address
+    {
+        public string Street1 { get; private set; }
+
+        public string Street2 { get; private set; }
+
+        public string City { get; private set; }
+
+        public string State { get; private set; }
+
+        public string ZipCode { get; private set; }
+
+        public string Country { get; private set; }
+
+        private Address() { }
+
+        public Address(string street1, string street2, string city, string state, string zipCode, string country)
+        {
+            Street1 = street1;
+            Street2 = street2;
+            City = city;
+            State = state;
+            ZipCode = zipCode;
+            Country = country;
+        }
+    }
+}
---
+++ Infrastructure/Data/ModelConfiguration/LocationConfig.cs
@@ -0,0 +1,44 @@
+﻿using Edgias.Inventory.Management.ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Edgias.Inventory.Management.Infrastructure.Data.ModelConfiguration
+{
+    internal class LocationConfig : IEntityTypeConfiguration<Location>
+    {
+        public void Configure(EntityTypeBuilder<Location> builder)
+        {
+            builder.HasKey(l => l.Id);
+
+            builder.Property(l => l.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(l => l.Name)
+                .HasMaxLength(256)
+                .IsRequired();
+
+            builder.OwnsOne(l => l.LocationAddress, a =>
+            {
+                a.WithOwner();
+
+                a.Property(a => a.ZipCode)
+                    .HasMaxLength(18);
+
+                a.Property(a => a.Street1)
+                    .HasMaxLength(180);
+
+                a.Property(a => a.Street2)
+                    .HasMaxLength(180);
+
+                a.Property(a => a.State)
+                    .HasMaxLength(60);
+
+                a.Property(a => a.Country)
+                    .HasMaxLength(90);
+
+                a.Property(a => a.City)
+                    .HasMaxLength(100);
+            });
+        }
+    }
+}
